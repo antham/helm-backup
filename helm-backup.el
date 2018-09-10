63409974193e51613c181cbddc40dea735db155b
Anthony HAMON <anthony.hamon@iadvize.com>
2018-09-11 00:14:35 +0200

    Update version and copyright

--- helm-backup.el
+++ helm-backup.el
@@ -1,10 +1,10 @@
 ;;; helm-backup.el --- Backup each file change using git -*- lexical-binding: t -*-
 
-;; Copyright (C) 2013-2017 Anthony HAMON
+;; Copyright (C) 2013-2018 Anthony HAMON
 
 ;; Author: Anthony HAMON <hamon.anth@gmail.com>
 ;; URL: http://github.com/antham/helm-backup
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((helm "1.5.5") (s "1.8.0") (cl-lib "0"))
 ;; Keywords: backup, convenience, files, tools, vc
 
